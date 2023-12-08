c569f0dd334cb206f389c169b7ea2178bcf351fb
zzziCode <1980136696@qq.com>
2023-12-08 09:09:13 +0800

    添加redis相关配置

--- src/test/java/com/zzzi/reggie/ReggieApplicationTests.java
+++ src/test/java/com/zzzi/reggie/ReggieApplicationTests.java
@@ -3,7 +3,13 @@ package com.zzzi.reggie;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import redis.clients.jedis.Jedis;
-
+/**@author zzzi
+ * @date 2023/12/7 19:02
+ * HashMap中解决冲突的链表长度大于8时，会尝试将链表改造成红黑树，但是前提是数组的大小超过64
+ * 没有超过64时会优先对数组进行扩容，扩容之后有了新的数组长度，冲突的情况就会变化
+ * 当超过负载因子时就会扩容
+ * 扩容涉及到重新哈希，
+ */
 //@SpringBootTest
 class ReggieApplicationTests {
 
