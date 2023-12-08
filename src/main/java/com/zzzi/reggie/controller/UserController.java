6d5ebbe1307aac05a1e04beb4647f50f134f2103
zzziCode <1980136696@qq.com>
2023-12-08 09:24:46 +0800

    使用redis缓存验证码，优化登录方式

--- src/main/java/com/zzzi/reggie/controller/UserController.java
+++ src/main/java/com/zzzi/reggie/controller/UserController.java
@@ -7,10 +7,12 @@ import com.zzzi.reggie.service.UserService;
 import com.zzzi.reggie.utils.ValidateCodeUtils;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpSession;
 import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
 @RestController
 @RequestMapping("/user")
@@ -18,6 +20,9 @@ import java.util.Map;
 public class UserController {
     @Autowired
     private UserService userService;
+
+    @Autowired
+    private RedisTemplate redisTemplate;
 
     //处理前端用户登录的方法
     @PostMapping("/login")
@@ -27,24 +32,28 @@ public class UserController {
         //根据手机号获取到当时生成的验证码，与现在接收到的验证码进行验证对比
         log.info("用户session为:{}", session.getAttribute(map.get("phone")));
 
-        String validCode = (String) session.getAttribute(map.get("phone"));
-
+        //从redis中获取保存的验证码
+        //String validCode = (String) session.getAttribute(map.get("phone"));
+        String phone = map.get("phone");
+        Object validCode = redisTemplate.opsForValue().get(phone);
         if (validCode != null && validCode.equals(map.get("code"))) {
-            //还要给当前用户保存一个session，一旦登陆成功，过滤器查询到这个session，就不会拦截了
+            //一旦登陆成功，就删除redis中缓存的验证码
+            redisTemplate.delete(phone);
 
             //如果是新用户，还需要自动注册，也就是将其保存到用户表中
             LambdaQueryWrapper<User> queryWrapper = new LambdaQueryWrapper<>();
-            queryWrapper.eq(User::getPhone, map.get("phone"));
+            queryWrapper.eq(User::getPhone, phone);
 
             User user = userService.getOne(queryWrapper);
 
             //没查询到，说明是一个新用户
             if (user == null) {
                 user = new User();
-                user.setPhone(map.get("phone"));
+                user.setPhone(phone);
                 userService.save(user);
 
             }
+            //将已登录的用户id保存到session中
             session.setAttribute("user", user.getId());
 
             return R.success(user);
@@ -57,19 +66,22 @@ public class UserController {
     public R<String> sendMsg(@RequestBody User user, HttpSession session) {
         log.info("用户的信息为：{}", user);
 
+        String phone = user.getPhone();
+
         //传递了手机号才发送验证码
-        if (user.getPhone() != null) {
+        if (phone != null) {
             //生成验证码，将其保存到session中
             String code = ValidateCodeUtils.generateValidateCode(4).toString();
 
             log.info("生成的验证码为：{}", code);
 
             //将生成的验证码及其对应的手机号形成一个键值对保存到session中
-            session.setAttribute(user.getPhone(), code);
+            //session.setAttribute(user.getPhone(), code);
+
+            //将验证码保存到redis中，并且设置过期时间为五分钟
+            redisTemplate.opsForValue().set(phone, code, 1, TimeUnit.MINUTES);
             return R.success("验证码生成成功");
         }
         return R.error("验证码发送失败");
-
-
     }
 }
