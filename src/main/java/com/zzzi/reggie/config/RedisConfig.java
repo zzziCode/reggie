115512d6b0205c805e70314592cfa9abbe3e4709
zzziCode <1980136696@qq.com>
2023-12-08 10:33:51 +0800

    使用redis进行缓存优化

---
+++ src/main/java/com/zzzi/reggie/config/RedisConfig.java
@@ -0,0 +1,22 @@
+package com.zzzi.reggie.config;
+
+import org.springframework.cache.annotation.CachingConfigurerSupport;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Configuration
+public class RedisConfig extends CachingConfigurerSupport {
+
+    //Redis的相关配置，配置好之后自己创建一个redisTemplate对象
+    //设置序列化器
+    @Bean
+    public RedisTemplate<Object, Object> redisTemplate(RedisConnectionFactory connectionFactory) {
+        RedisTemplate<Object, Object> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setConnectionFactory(connectionFactory);
+        return redisTemplate;
+    }
+}
