ea5abfe02eb13d2b24b7ae966587ddb8526d7e09
zzziCode <1980136696@qq.com>
2023-12-09 15:33:48 +0800

    使用redis对套餐功能进行优化

--- src/main/java/com/zzzi/reggie/ReggieApplication.java
+++ src/main/java/com/zzzi/reggie/ReggieApplication.java
@@ -4,12 +4,15 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @Slf4j
 @SpringBootApplication
 @ServletComponentScan//开启这个注解才能支持mvc中的过滤器
 @EnableTransactionManagement
+//开启缓存的功能
+@EnableCaching
 public class ReggieApplication {
     public static void main(String[] args) {
         SpringApplication.run(ReggieApplication.class,args);
